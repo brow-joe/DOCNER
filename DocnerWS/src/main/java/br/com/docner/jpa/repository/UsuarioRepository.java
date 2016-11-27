2b1a06f679cecc8b76b14365ce9eaf786cef66e5
brow-joe <jo_souza92@yahoo.com.br>
2016-11-27 13:00:35 -0200

    drop

--- DocnerWS/src/main/java/br/com/docner/jpa/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.docner.jpa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.docner.jpa.entity.Usuario;
-
-public interface UsuarioRepository extends JpaRepository< Usuario, Long >{
-	public Usuario findByUsuario( String usuario );
-}
